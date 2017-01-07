36e29817b768b4c8ee08a056a4862dde4f5d7f1c
SlavaPihidko <slava17puh@gmail.com>
2017-01-07 22:49:42 +0200

    Создание класса RestDataOfMainPage
    для заполнения данными главной страницы ресторана

---
+++ Lunchpoint/src/test/java/restaurants/RestDataOfMainPage.java
@@ -0,0 +1,13 @@
+package restaurants;
+
+public class RestDataOfMainPage {
+  private final String nameOfRest;
+
+  public RestDataOfMainPage(String nameOfRest) {
+    this.nameOfRest = nameOfRest;
+  }
+
+  public String getNameOfRest() {
+    return nameOfRest;
+  }
+}
