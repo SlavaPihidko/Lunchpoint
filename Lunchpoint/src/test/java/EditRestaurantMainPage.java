9612a2ec209cc217ac1568fdf6e2fc0420348b72
SlavaPihidko <slava17puh@gmail.com>
2017-01-07 20:50:31 +0200

    Улучшение первого сценария EditRestaurantMainPage
    Улучшение первого сценария

--- Lunchpoint/src/test/java/EditRestaurantMainPage.java
+++ Lunchpoint/src/test/java/EditRestaurantMainPage.java
@@ -36,13 +36,13 @@ public class EditRestaurantMainPage {
         Thread.sleep(3000);
         wd.findElement(By.name("RestaurantsLangSearch[name]")).click();
         wd.findElement(By.name("RestaurantsLangSearch[name]")).clear();
-        wd.findElement(By.name("RestaurantsLangSearch[name]")).sendKeys("test");
-        wd.get("http://lptest.bigdig.com.ua/manager/restaurants?RestaurantsLangSearch%5Bname%5D=test&RestaurantsLangSearch%5Baddress%5D=");
-        /*wd.findElement(By.name("RestaurantsLangSearch[name]")).click();
+        wd.findElement(By.name("RestaurantsLangSearch[name]")).sendKeys("slava");
+       // wd.get("http://lptest.bigdig.com.ua/manager/restaurants?RestaurantsLangSearch%5Bname%5D=test&RestaurantsLangSearch%5Baddress%5D=");
+        wd.findElement(By.name("RestaurantsLangSearch[name]")).click();
         wd.findElement(By.name("RestaurantsLangSearch[address]")).click();
-        wd.findElement(By.name("RestaurantsLangSearch[address]")).clear();*/
+        //wd.findElement(By.name("RestaurantsLangSearch[address]")).clear();
         Thread.sleep(3000);
-        wd.findElement(By.cssSelector("a[href='/manager/restaurants/update?id=2229']>span.glyphicon.glyphicon-pencil")).click();
+        wd.findElement(By.cssSelector("a[href='/manager/restaurants/update?id=2228']>span.glyphicon.glyphicon-pencil")).click();
         wd.findElement(By.id("edit_restName")).click();
         Thread.sleep(3000);
         wd.findElement(By.id("edit_restName")).clear();
