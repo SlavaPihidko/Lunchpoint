7f9c046341ec7fb21d26e43759d844faf9a350ca
SlavaPihidko <slava17puh@gmail.com>
2017-01-30 13:41:04 +0200

    создан класс SiteHelper для манипуляций на стороне сайта на главной странице

---
+++ Lunchpoint/src/test/java/restaurants/appmanager/SiteHelper.java
@@ -0,0 +1,13 @@
+package restaurants.appmanager;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Slava on 30.01.2017.
+ */
+public class SiteHelper extends HelperBase {
+
+  public SiteHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+}
