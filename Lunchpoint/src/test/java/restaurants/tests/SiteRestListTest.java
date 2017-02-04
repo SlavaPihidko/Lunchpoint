92a8fc1de25eac156efd6aedced9cec1a70e013c
SlavaPihidko <slava17puh@gmail.com>
2017-02-04 15:14:16 +0200

    Добавлена проверка на наличие картинки в списке ресторанов на сайте

--- Lunchpoint/src/test/java/restaurants/tests/SiteRestListTest.java
+++ Lunchpoint/src/test/java/restaurants/tests/SiteRestListTest.java
@@ -24,11 +24,21 @@ public class SiteRestListTest extends TestBase {
             .text(By.cssSelector("div[lng='30.39818839999998'] > div.content > div.address")), "вул.Жолудєва 8");
     assertEquals(app.getMainPageHelper()
             .text(By.cssSelector("div[lng='30.39818839999998'] > div.content div.min-bill.clear > span")), "<100 грн");
+
+    // количество 29 символов потому что /manager/img/restaurants/.jpg = 29 символов
+    // когда заглушка, то атрибут img будет короткий, к примеру /img/3.png
+    // проверка на наличие главной картинки
+    String attributeOfImg = app.getMainPageHelper().attribute(By.cssSelector("div[lng='30.39818839999998'] > a.img-outer"), "img");
+    System.out.println(attributeOfImg);
+    int n = 29;
+    System.out.println(attributeOfImg.length());
+     if(attributeOfImg.length() < n){
+       assertEquals(app.getMainPageHelper()
+               .attribute(By.cssSelector("div[lng='30.39818839999998'] > a.img-outer"), "img"), "testMustFail");
+     }
+    // если заглушка вместо картинки
     /*assertEquals(app.getMainPageHelper()
-            .attribute(By.cssSelector("div[lng='30.39818839999998'] > a[img='/img/3.png']"), "img"), "/img/3.png"); */ // если заглушка вместо картинки
-    //System.out.print(app.getMainPageHelper().attribute(By.cssSelector("div[lng='30.39818839999998'] > a[img='/img/3.png']"), "img")); // выводим то что возвращает метод attribute
+            .attribute(By.cssSelector("div[lng='30.39818839999998'] > a.img-outer"), "img"), "/img/3.png"); */
+    //System.out.print(app.getMainPageHelper().attribute(By.cssSelector("div[lng='30.39818839999998'] > a.img-outer"), "img")); // выводим то что возвращает метод attribute
   }
-
-
-
 }
