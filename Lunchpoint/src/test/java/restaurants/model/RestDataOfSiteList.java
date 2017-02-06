09c85553779751fb219893f4d9945a78cd7de877
SlavaPihidko <slava17puh@gmail.com>
2017-02-07 01:20:22 +0200

    Успешный тест проверяет потипу заведения, все ли выбраны заведения имеют конкретный тип

--- Lunchpoint/src/test/java/restaurants/model/RestDataOfSiteList.java
+++ Lunchpoint/src/test/java/restaurants/model/RestDataOfSiteList.java
@@ -11,4 +11,31 @@ public class RestDataOfSiteList {
 
     this.nameOfRestList = nameOfRestList;
   }
+
+  public String getNameOfRestList() {
+    return nameOfRestList;
+  }
+
+  @Override
+  public String toString() {
+    return "RestDataOfSiteList{" +
+            "nameOfRestList='" + nameOfRestList + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    RestDataOfSiteList that = (RestDataOfSiteList) o;
+
+    return nameOfRestList != null ? nameOfRestList.equals(that.nameOfRestList) : that.nameOfRestList == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    return nameOfRestList != null ? nameOfRestList.hashCode() : 0;
+  }
 }
--- Lunchpoint/src/test/java/restaurants/tests/SelectingTypeOfRestTests.java
+++ Lunchpoint/src/test/java/restaurants/tests/SelectingTypeOfRestTests.java
@@ -1,9 +1,14 @@
 package restaurants.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import restaurants.model.RestDataOfSiteList;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by Slava on 06.02.2017.
@@ -13,10 +18,18 @@ public class SelectingTypeOfRestTests extends TestBase {
   @Test
   public void testSelectingTypeOfRest() throws InterruptedException {
     app.getSiteHelper().choiceTheTown("Харків");
-    //app.getSiteHelper().scroll("scrollBy(0,100)");
-    //app.getSiteHelper().goToRestTab();
     // Выбор Типа Бар
     app.getSiteHelper().choiceTypeOfRest("type1");
-    List<RestDataOfSiteList> before = app.getSiteHelper().getRestList();
+    List<RestDataOfSiteList> objectWithOnlyTypeFromWeb = app.getSiteHelper().getRestList();
+
+    List<RestDataOfSiteList> objectWithHardType = new ArrayList<RestDataOfSiteList>();
+    for(int i=0; i<objectWithOnlyTypeFromWeb.size();i++) {
+      String typeOfRest = "Бар";
+      RestDataOfSiteList rest = new RestDataOfSiteList(typeOfRest);
+      objectWithHardType.add(rest);
+    }
+
+    for(int i=0; i<objectWithOnlyTypeFromWeb.size();i++) {
+      assertEquals(objectWithOnlyTypeFromWeb.get(i), objectWithHardType.get(i));}
   }
 }
