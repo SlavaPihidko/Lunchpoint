7c386d67aa7413cc31330d910c70ced4cf72aa6d
SlavaPihidko <slava17puh@gmail.com>
2017-02-10 00:32:49 +0200

    Реализован тест для проверки логина суперадмина

---
+++ Lunchpoint/src/test/java/restaurants/tests/LoginTests.java
@@ -0,0 +1,27 @@
+package restaurants.tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Slava on 09.02.2017.
+ */
+public class LoginTests extends TestBase {
+
+  @Test
+  public void loginManagerTestPos (){
+    app.getSessionHelper().login(usernameAdmin, passwordAdmin);
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-top.clear div.wrap div.to-right > a.user-profile-link")), true);
+    assertEquals(app.getMainPageHelper()
+            .attribute(By.cssSelector("div.header-top.clear div.wrap div.to-right > a.user-profile-link"),"href"),"http://lptest.bigdig.com.ua/manager/restaurants");
+    assertEquals(app.getMainPageHelper()
+            .text(By.cssSelector("div.header-top.clear div.wrap div.to-right > a.user-profile-link")), "MANAGER");
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-top.clear div.wrap div.to-right > a.user-profile-link > i.fa.fa-user")),true);
+
+  }
+}
