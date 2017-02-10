c17109ae49898ff667b6b5798e76eb93a4446f3c
SlavaPihidko <slava17puh@gmail.com>
2017-02-11 00:50:23 +0200

    Реализ тестлв наличия елементов 2

--- Lunchpoint/src/test/java/restaurants/tests/PresenceElementsMainPage.java
+++ Lunchpoint/src/test/java/restaurants/tests/PresenceElementsMainPage.java
@@ -3,6 +3,7 @@ package restaurants.tests;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static java.sql.Types.NULL;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -11,8 +12,9 @@ import static org.testng.Assert.assertTrue;
  */
 public class PresenceElementsMainPage extends TestBase {
 
-  @Test
-  public void presenceElementsHeader(){
+
+  @Test(enabled = false)
+  public void presenceElementsHeaderTop(){
     // наличие логотипа ЛП
     assertEquals(app.getMainPageHelper()
             .elementPresent(By.cssSelector("div.header-top.clear div.wrap > a > img[src='/img/logo-home1.svg'] ")),true);
@@ -37,6 +39,55 @@ public class PresenceElementsMainPage extends TestBase {
     // текст кнопки Присоедини ресторан
     assertEquals(app.getMainPageHelper()
             .text(By.cssSelector("div.header-top.clear div.wrap div.to-right a.btn")),"ПРИСОЕДИНИ РЕСТОРАН");
+
+  }
+
+  @Test
+  public void presenceElementsHeaderMiddle(){
+    // наличие текста
+    assertEquals(app.getMainPageHelper()
+            .text(By.cssSelector("div.header-search div.phrace")),"ПРОГОЛОДАЛСЯ? МЕНЮ В ОДИН КЛИК!");
+    // наличие формы
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-search div.wrap form#w0")),true);
+    // наличие поля для ввода адреса
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-search div.wrap form#w0 > div.input-outer.location > div#cities")),true);
+    // наличие пульсируещого значка
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-search div.wrap form#w0 > div.input-outer.location > div#cities >a.check-location.pulsing")),true);
+    // наличие текста по умолчанию в поле для ввода города
+    String s = app.getMainPageHelper()
+            .attribute(By.cssSelector("div.header-search div.wrap form#w0 > div.input-outer.location > div#cities input#name_address"),"value");
+    System.out.println(s);
+    if (s == "") {
+      // если текста по умолчанию нету в поле то тест упадет
+      assertEquals(app.getMainPageHelper()
+              .attribute(By.cssSelector("div.header-search div.wrap form#w0 > div.input-outer.location > div#cities input#name_address"),"value"),"mustFail");
+    }
+
+    // наличие птички в поле для ввода города
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-search div.wrap form#w0 > div.input-outer.location > div#cities i.fa.fa-angle-down")),true);
+
+
+    // наличие поля для ввода ресторана
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-search div.wrap form#w0 div#customSelectSearch")),true);
+    // текст плейсхолдера в поле ввода ресторана
+    assertEquals(app.getMainPageHelper()
+            .attribute(By.cssSelector("div.header-search div.wrap form#w0 div#customSelectSearch > input"),"placeholder"),"Выберите раздел или укажите название ресторана или кухни...");
+    // наличие птички в поле для ввода ресторана
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-search div.wrap form#w0 div#customSelectSearch i.fa.fa-angle-down.rotated.collapsed")),true);
+    // наличие кнопки Поиска
+    assertEquals(app.getMainPageHelper()
+            .elementPresent(By.cssSelector("div.header-search div.wrap form#w0 div.input-outer.to-right")),true);
+
+    assertEquals(app.getMainPageHelper()
+            .attribute(By.cssSelector("div.header-search div.wrap form#w0 div.input-outer.to-right > input"), "value"),"Поиск");
+
+
 
   }
 
