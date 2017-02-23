a6b74d42786a66f42b98f06ed0c109b1fa7ee7ac
SlavaPihidko <slava17puh@gmail.com>
2017-02-23 21:36:38 +0200

    Реализован тест на ввод  м дальнейшую проверку разных значений в поле названия ресторана

--- Lunchpoint/src/test/java/restaurants/tests/NameOfRestTests.java
+++ Lunchpoint/src/test/java/restaurants/tests/NameOfRestTests.java
@@ -18,25 +18,37 @@ public class NameOfRestTests extends TestBase {
             "slava ltd",
             "фотограф",
             "abc",
+            "слава ?/\\|,.!@#$%^&*()-=+)_",
             "aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaa",
             "slava123"};
+    // вконце создаем с именем slava что бы вернуть в состояние по умолчанию
     String[] nameDataOfRestNegative = {"aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaa1", "slava"};
     for (int i = 0; i < nameDataOfRest.length; i++) {
       app.getAdminHelper().getAddressMainUrl();
       app.getSiteHelper().enterRestName(nameDataOfRest[i]);
       app.getSiteHelper().searchRestButton();
+      // проверяем на сайте в списке что имя ресторана такое как ему изменили
       assertEquals(app.getMainPageHelper()
               .text(By.cssSelector("div[lng='30.39818839999998'] div.title")),nameDataOfRest[i]) ;
+      app.getSiteHelper().click(By.cssSelector("div[lng='30.39818839999998'] div.title"));
+      // проверяем на сайте на страничке ресторана
+      assertEquals(app.getMainPageHelper()
+              .text(By.cssSelector("div.cafe-name")), nameDataOfRest[i]);
+      // переход в админку
       app.getMainPageHelper().click(By.cssSelector("div.header-top.clear div.wrap div.to-right > a.user-profile-link"));
       app.getAdminHelper().searchRestInAdminPanel("RestaurantsLangSearch[name]", nameDataOfRest[i]);
       app.getAdminHelper().gotoAddressFieldInAdminPanel();
-
+      // Проверяем на налие ресторана с указаным id
       assertEquals(app.getAdminHelper()
               .elementPresent(By.cssSelector("a[href='/manager/restaurants/update?id=2219']")), true);
       app.getAdminHelper().gotoEditRestInAdminPanel();
-      //Thread.sleep(2000);
+      // проверяем в админке на главной страничке ресторана в поле ввода названия ресторана что имя такое как перед этим записали
       assertEquals(app.getMainPageHelper()
               .attribute(By.id("edit_restName"),"value"), nameDataOfRest[i]);
+      // проверяем в админке на главной страничке ресторана в шапке ресторана что имя такое как перед этим записали
+      assertEquals(app.getMainPageHelper()
+              .text(By.cssSelector("div.cafe-name")),nameDataOfRest[i]);
+      // делаем цикл по массиву
       if (i != nameDataOfRest.length - 1) {
         app.getMainPageHelper().fillNameOfRest(nameDataOfRest, i + 1);
         app.getMainPageHelper().saveRestMainPage();
@@ -51,6 +63,7 @@ public class NameOfRestTests extends TestBase {
     }
     app.getAdminHelper().searchRestInAdminPanel("RestaurantsLangSearch[name]", "slava123");
     app.getAdminHelper().gotoAddressFieldInAdminPanel();
+    // делаем цикл для ввода негативных значений
     for (int y = 0; y < nameDataOfRestNegative.length; y++) {
       if(y!=0) {
         app.getAdminHelper().searchRestInAdminPanel("RestaurantsLangSearch[name]", "aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaa");
@@ -64,6 +77,7 @@ public class NameOfRestTests extends TestBase {
        if(y != nameDataOfRestNegative.length - 1) {
         app.getAdminHelper().searchRestInAdminPanel("RestaurantsLangSearch[name]", "aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaa1");
         app.getAdminHelper().gotoAddressFieldInAdminPanel();
+         // проверяем что после ввода негативного значения ничего не найдено
         assertEquals(app.getAdminHelper()
                 .text(By.cssSelector("table.table.table-striped.table-bordered div.empty")), "Ничего не найдено.");
       }
