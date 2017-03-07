317217c8e2e9bc7087d9f31195053d2a7d4ec44f
SlavaPihidko <slava17puh@gmail.com>
2017-03-07 23:27:03 +0200

    Реализован тест для проверки ресторана по типу

---
+++ Lunchpoint/src/test/java/restaurants/tests/TypeOfRestTests.java
@@ -0,0 +1,32 @@
+package restaurants.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Slava on 07.03.2017.
+ */
+public class TypeOfRestTests extends TestBase {
+
+  @Test
+  public void typeOfRestTest() throws InterruptedException {
+    app.getSessionHelper().login(usernameAdmin, passwordAdmin);
+    Thread.sleep(2000);
+
+    app.getSiteHelper().searchRestOnTheSite();
+    // проверяем на сайте в списке что тип ресторана такой как ему изменили
+    assertEquals(app.getMainPageHelper()
+            .text(By.cssSelector("div[lng='30.39818839999998'] > div.content > div.type")),"Бистро");
+    app.getSiteHelper().click(By.cssSelector("div[lng='30.39818839999998'] div.title"));
+    // проверка по типу ресторана на страничке ресторана
+    assertEquals(app.getMainPageHelper()
+            .text(By.cssSelector("div.wrap div.cafe-type > p")), "Бистро");
+
+    app.getAdminHelper().getAddressMainUrl("manager/restaurants/update?id=2219");
+    // проверяем в админке на главной страничке ресторана в поле ввода названия ресторана что имя такое как перед этим записали
+    assertEquals(app.getMainPageHelper()
+            .text(By.cssSelector("span#select2-restType-container")), restEditDataOfMainPageAllField.getTypeOfRest());
+  }
+}
