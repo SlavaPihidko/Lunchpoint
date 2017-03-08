724f6a591033ab2e678185c03b166349ba9eacd5
SlavaPihidko <slava17puh@gmail.com>
2017-03-08 20:31:01 +0200

    Реализован тест для проверки Дополнительныз услуг

---
+++ Lunchpoint/src/test/java/restaurants/tests/AdditionalServicesTests.java
@@ -0,0 +1,21 @@
+package restaurants.tests;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Slava on 08.03.2017.
+ */
+public class AdditionalServicesTests extends TestBase {
+
+  @Test
+  public void additionalServicesTest() throws InterruptedException {
+
+    app.getAdminHelper().getAddressMainUrl("slavkotest123");
+    Thread.sleep(2000);
+    assertEquals(app.getMainPageHelper()
+            .text(By.cssSelector("div.features-outer div.features div[dir='ltr'] > li.wi-fi:first-of-type")),
+            restEditDataOfMainPageAllField.getAdditionalServiceOfRest());
+  }
+}
