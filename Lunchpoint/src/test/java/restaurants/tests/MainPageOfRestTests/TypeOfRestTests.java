2c1554de142c0e64d8af36a6bc16948e592745c9
SlavaPihidko <slava17puh@gmail.com>
2017-03-27 00:54:38 +0300

    Улучшение в тесте по типу ресторана

--- Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/TypeOfRestTests.java
+++ Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/TypeOfRestTests.java
@@ -6,6 +6,7 @@ import restaurants.tests.TestBase;
 import restaurants.utils.MainPgUtils;
 
 import static org.testng.Assert.assertEquals;
+import static restaurants.utils.MainPgUtils.restEditDataOfMainPg;
 
 /**
  * Created by Slava on 07.03.2017.
@@ -20,15 +21,23 @@ public class TypeOfRestTests extends TestBase {
     app.getSiteHelper().searchRestOnTheSite();
     // проверяем на сайте в списке что тип ресторана такой как ему изменили
     assertEquals(app.getMainPgHelper()
-            .text(By.cssSelector("div[lng='30.39818839999998'] > div.content > div.type")),"Бистро");
-    app.getSiteHelper().click(By.cssSelector("div[lng='30.39818839999998'] div.title"));
+            .text(By.cssSelector(String
+                    .format("div[lng='%s'] > div.content > div.type", restEditDataOfMainPg.getLng()))),"Бистро");
+
+    app.getSiteHelper()
+            .click(By.cssSelector(String.format("div[lng='%s'] div.title", restEditDataOfMainPg.getLng())));
     // проверка по типу ресторана на страничке ресторана
     assertEquals(app.getMainPgHelper()
             .text(By.cssSelector("div.wrap div.cafe-type > p")), "Бистро");
 
-    app.getAdminHelper().getAddressMainUrl("manager/restaurants/update?id=2219");
-    // проверяем в админке на главной страничке ресторана в поле ввода названия ресторана что имя такое как перед этим записали
+    app.getAdminHelper()
+            .getAddressMainUrl(String.format("manager/restaurants/update?id=%s", restEditDataOfMainPg.getId()));
+    // проверяем в админке на главной страничке ресторана в поле ввода названия ресторана
+    // что тип такой как перед этим записали
     assertEquals(app.getMainPgHelper()
-            .text(By.cssSelector("span#select2-restType-container")), MainPgUtils.restEditDataOfMainPg.getTypeOfRest());
+            .text(By.cssSelector("span#select2-restType-container")), restEditDataOfMainPg.getTypeOfRest());
+    // проверяем в админке на главной страничке ресторана в шапке
+    assertEquals(app.getMainPgHelper()
+            .text(By.cssSelector("div.row-footer.clear div.cafe-type >p")), restEditDataOfMainPg.getTypeOfRest());
   }
 }
