c52677f0ccf3a23dd693b6f6e68c4ec5dc7677c8
SlavaPihidko <slava17puh@gmail.com>
2017-03-29 21:49:00 +0300

    Улучшен тест для проверки типов ресторана

--- Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/TypeOfCuisineOfRestTests.java
+++ Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/TypeOfCuisineOfRestTests.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import restaurants.tests.TestBase;
 import restaurants.utils.MainPgUtils;
+import restaurants.utils.TypeOfCuisineUtils;
 
 import static org.testng.Assert.assertEquals;
+import static restaurants.utils.MainPgUtils.restEditDataOfMainPg;
+import static restaurants.utils.TypeOfCuisineUtils.*;
 
 /**
  * Created by Slava on 07.03.2017.
@@ -21,14 +24,15 @@ public class TypeOfCuisineOfRestTests extends TestBase {
     app.getSiteHelper().click(By.cssSelector("div[lng='30.39818839999998'] div.title"));
     // проверка по типу кухни ресторана на страничке ресторана
     assertEquals(app.getMainPgHelper()
-            .text(By.cssSelector("div.wrap div.cafe-kitchen > p")), "BBQ, Smoked food");
+            .text(By.cssSelector("div.wrap div.cafe-kitchen > p")), typeOfCuisine[0] +", "+ typeOfCuisine[1]);
+
 
     app.getAdminHelper().getAddressMainUrl("manager/restaurants/update?id=2219");
     Thread.sleep(2000);
     // проверяем в админке на главной страничке ресторана проверяем что типы кухонь такие как перед этим записали
     assertEquals(app.getMainPgHelper()
-            .attribute(By.cssSelector("div.input-outer.hint-cuisine span.selection li.select2-selection__choice:first-of-type"),"title"), MainPgUtils.restEditDataOfMainPg.getTypeOfCuisine());
+            .attribute(By.cssSelector("div.input-outer.hint-cuisine span.selection li.select2-selection__choice:first-of-type"),"title"), typeOfCuisine[0]);
     assertEquals(app.getMainPgHelper()
-            .attribute(By.cssSelector("div.input-outer.hint-cuisine span.selection li.select2-selection__choice:nth-of-type(2)"),"title"), "Smoked food");
+            .attribute(By.cssSelector("div.input-outer.hint-cuisine span.selection li.select2-selection__choice:nth-of-type(2)"),"title"), typeOfCuisine[1]);
   }
 }
---
+++ Lunchpoint/src/test/java/restaurants/utils/TypeOfCuisineUtils.java
@@ -0,0 +1,8 @@
+package restaurants.utils;
+
+/**
+ * Created by Slava on 29.03.2017.
+ */
+public class TypeOfCuisineUtils {
+  public static String typeOfCuisine [] =  {"BBQ", "Smoked food"};
+}
