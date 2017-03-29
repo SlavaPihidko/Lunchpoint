7767a42f772712c403b48d231dc34720bf62cf36
SlavaPihidko <slava17puh@gmail.com>
2017-03-29 16:06:25 +0300

    Добавление проверок в тест для прверки SEO URL

--- Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/SeoUrlTests.java
+++ Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/SeoUrlTests.java
@@ -17,8 +17,17 @@ public class SeoUrlTests extends TestBase {
   @Test
   public void seoUrlTest() throws InterruptedException {
 
+    app.getSessionHelper().login(usernameAdmin, passwordAdmin);
+    Thread.sleep(2000);
     app.getAdminHelper().getAddressMainUrl(restEditDataOfMainPg.getSeoUrlOfRest());
+    // Проверка на страничке ресторана что имя ресторана соответствует переходу по ЮРЛу
     assertEquals(app.getMainPgHelper()
             .text(By.cssSelector("div.cafe-name")), restEditDataOfMainPg.getNameOfRest());
+
+    app.getAdminHelper()
+            .getAddressMainUrl(String.format("manager/restaurants/update?id=%s", restEditDataOfMainPg.getId()));
+    // Проверка в админке в поле SEO url
+    assertEquals(app.getMainPgHelper()
+            .attribute(By.id("edit_seo_url"), "value" ),restEditDataOfMainPg.getSeoUrlOfRest());
   }
 }
