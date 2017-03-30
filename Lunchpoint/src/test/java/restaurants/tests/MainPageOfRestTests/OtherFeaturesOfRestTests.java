3d3126f084008cb25c8f9d81f3d173dcdb2769bd
SlavaPihidko <slava17puh@gmail.com>
2017-03-30 21:57:50 +0300

    Улучшения в otherFeatures

--- Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/OtherFeaturesOfRestTests.java
+++ Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/OtherFeaturesOfRestTests.java
@@ -6,6 +6,7 @@ import restaurants.tests.TestBase;
 import restaurants.utils.MainPgUtils;
 
 import static org.testng.Assert.assertEquals;
+import static restaurants.utils.MainPgUtils.restEditDataOfMainPg;
 
 /**
  * Created by Slava on 08.03.2017.
@@ -14,10 +15,10 @@ public class OtherFeaturesOfRestTests extends TestBase {
   @Test
   public void additionalServicesTest() throws InterruptedException {
 
-    app.getAdminHelper().getAddressMainUrl("slavkotest123");
+    app.getAdminHelper().getAddressMainUrl(restEditDataOfMainPg.getSeoUrlOfRest());
     Thread.sleep(2000);
     assertEquals(app.getMainPgHelper()
                     .text(By.cssSelector("div.features-outer div.features div[dir='ltr'] > li.wi-fi:nth-of-type(2)")),
-            MainPgUtils.restEditDataOfMainPg.getOtherFeaturesOfRest());
+            restEditDataOfMainPg.getOtherFeaturesOfRest());
   }
 }
