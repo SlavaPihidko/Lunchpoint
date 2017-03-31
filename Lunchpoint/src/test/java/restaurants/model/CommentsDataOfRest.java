96c80df31d5823ff8a0b78e031ec77f60a91d5d2
SlavaPihidko <slava17puh@gmail.com>
2017-03-31 17:33:43 +0300

    Начало создания теста для проверки комментариев в ресторане

---
+++ Lunchpoint/src/test/java/restaurants/model/CommentsDataOfRest.java
@@ -0,0 +1,13 @@
+package restaurants.model;
+
+/**
+ * Created by Slava on 31.03.2017.
+ */
+public class CommentsDataOfRest {
+  private int id;
+  private String comment;
+
+  public CommentsDataOfRest(int id, String comment){
+
+  }
+}
---
+++ Lunchpoint/src/test/java/restaurants/tests/MainPageOfRestTests/CommentsOfRest.java
@@ -0,0 +1,15 @@
+package restaurants.tests.MainPageOfRestTests;
+
+import org.testng.annotations.Test;
+import restaurants.tests.TestBase;
+
+/**
+ * Created by Slava on 31.03.2017.
+ */
+public class CommentsOfRest extends TestBase {
+
+  @Test
+  public void commentsOfRest() {
+
+  }
+}
