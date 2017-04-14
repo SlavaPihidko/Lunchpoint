fa9ba086fd7ec64ef141ce30b564b48cb5690640
SlavaPihidko <slava17puh@gmail.com>
2017-04-14 22:20:21 +0300

    Реализован сценарий запуска автотестов

--- Lunchpoint/src/test/java/restaurants/generators/RestDataOfMainPgGenerator.java
+++ Lunchpoint/src/test/java/restaurants/generators/RestDataOfMainPgGenerator.java
@@ -59,9 +59,9 @@ public class RestDataOfMainPgGenerator {
   private void saveAsJson(List<RestDataOfMainPage> dataOfRestsMainPg, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(dataOfRestsMainPg);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try(Writer writer = new FileWriter(file)) {  //поместили в try что бы автоматически закрывался файл на запись
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<RestDataOfMainPage> dataOfRestsMainPg, File file) throws IOException {
