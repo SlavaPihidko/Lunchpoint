e6505df7555b1ffa1ad6acbf943f8e4c56129fe7
SlavaPihidko <slava17puh@gmail.com>
2017-05-04 20:00:23 +0300

    практика доп.1

--- Lunchpoint/src/test/java/restaurants/tests/draft/ToDoMVCTest1.java
+++ Lunchpoint/src/test/java/restaurants/tests/draft/ToDoMVCTest1.java
@@ -22,7 +22,11 @@ import static com.thoughtworks.selenium.SeleneseTestNgHelper.assertEquals;
  */
 public class ToDoMVCTest1 {
   protected WebDriver wd;
-  int sizeOfList;
+  private int sizeOfList;
+  private String st1 = "Task1";;
+  private String st2 = "Task2";;
+  private String st3 = "Task3";;
+  private String st4 = "Task4";
 
   @BeforeSuite
   public void init() {
@@ -33,13 +37,13 @@ public class ToDoMVCTest1 {
   public void toDoMVCTest() throws InterruptedException {
     wd.get("http://todomvc.com/examples/troopjs_require/");
     wd.findElement(By.cssSelector("input.new-todo")).click();
-    wd.findElement(By.cssSelector("input.new-todo")).sendKeys("Task1" + Keys.ENTER);
+    wd.findElement(By.cssSelector("input.new-todo")).sendKeys(st1 + Keys.ENTER);
     wd.findElement(By.cssSelector("header.header input.new-todo")).click();
-    wd.findElement(By.cssSelector("header.header input.new-todo")).sendKeys("Task2" + Keys.ENTER);
+    wd.findElement(By.cssSelector("header.header input.new-todo")).sendKeys(st2 + Keys.ENTER);
     wd.findElement(By.cssSelector("header.header input.new-todo")).click();
-    wd.findElement(By.cssSelector("input.new-todo")).sendKeys("Task3" + Keys.ENTER);
+    wd.findElement(By.cssSelector("input.new-todo")).sendKeys(st3 + Keys.ENTER);
     wd.findElement(By.cssSelector("header.header input.new-todo")).click();
-    wd.findElement(By.cssSelector("input.new-todo")).sendKeys("Task4" + Keys.ENTER);
+    wd.findElement(By.cssSelector("input.new-todo")).sendKeys(st4 + Keys.ENTER);
   }
   @Test
   public void toDoMVCTest1() {
@@ -50,8 +54,15 @@ public class ToDoMVCTest1 {
     assertEquals(elements.size(), sizeOfList);
   }
 
-  @Test(dependsOnMethods = "toDoMVCTest1", alwaysRun = true)
-  public void toDoMVCTest2() throws InterruptedException {
+  @Test(dependsOnMethods ="toDoMVCTest1", alwaysRun = true)
+  public void toDoMVCTest2(){
+    String textOf4Element = wd.findElement(By.cssSelector("ul.todo-list li.active:nth-of-type(4)")).getText();
+    System.out.println(textOf4Element);
+    assertEquals(textOf4Element, st4);
+  }
+
+  @Test(dependsOnMethods = "toDoMVCTest2", alwaysRun = true)
+  public void toDoMVCTest3() throws InterruptedException {
     WebElement destroy2 = wd.findElement(By.cssSelector("ul.todo-list li.active:nth-of-type(2)"));
     Actions action = new Actions(wd);
     action.moveToElement(destroy2);
@@ -66,13 +77,25 @@ public class ToDoMVCTest1 {
     assertEquals(elements.size(), sizeOfList);
   }
 
-  @Test//(dependsOnMethods ="toDoMVCTest2", alwaysRun = true)
-  public void toDoMVCTest3() throws InterruptedException {
+  @Test(dependsOnMethods ="toDoMVCTest3", alwaysRun = true)
+  public void toDoMVCTest4() throws InterruptedException {
     wd.findElement(By.cssSelector("ul.todo-list li.active:last-of-type input.toggle")).click();
     Thread.sleep(3000);
     wd.findElement(By.cssSelector("a[href='#/completed']")).click();
     String firstCompletedItem = wd.findElement(By.cssSelector("ul.todo-list.filter-completed li.completed label")).getText();
-  assertEquals(firstCompletedItem, "Task4");
+    assertEquals(firstCompletedItem, st4);
+  }
+
+  @Test(dependsOnMethods = "toDoMVCTest4", alwaysRun = true)
+  public void toDoMVCTest5() throws InterruptedException {
+    wd.findElement(By.cssSelector("ul.todo-list li.active:last-of-type input.toggle")).click();
+    Thread.sleep(3000);
+    wd.findElement(By.cssSelector("a[href='#/active']")).click();
+    List<WebElement> elements = wd.findElements(By.cssSelector("ul.todo-list li"));
+    sizeOfList = new Integer(wd.findElement(By.cssSelector("span.todo-count > strong")).getText());
+    System.out.println("elements.size=" + elements.size());
+    System.out.println("sizeOfList =" + sizeOfList);
+    assertEquals(elements.size(), sizeOfList);
   }
 
   @AfterSuite
