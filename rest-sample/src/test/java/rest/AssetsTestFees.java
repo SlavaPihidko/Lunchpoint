632cd0db0c6101195fbe138080cf511e393d9daa
SlavaPihidko <slava17puh@gmail.com>
2018-08-13 19:16:37 +0300

    реализован Post запрос AssetsFees через готовый Json

--- rest-sample/src/test/java/rest/AssetsTestFees.java
+++ rest-sample/src/test/java/rest/AssetsTestFees.java
@@ -3,6 +3,12 @@ package rest;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.Test;
 
@@ -29,7 +35,18 @@ public class AssetsTestFees {
 
     private int updateAssetsFees(AssetsFees assetsFees) throws IOException {
 
-                String jsonPut = Request.Put("http://146.71.78.211/api/admin/fees")
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPut httpPost = new HttpPut("http://146.71.78.211/api/admin/fees");
+        String json = "{\"assets\": [{\"id\":1,\"trade_fee\": 2,\"deposit_fee\": 0.4,\"withdraw_fee\": 0.000006,\"order_max\": 0.4,\"order_min\": 400,\"deposit_max\": 0.4,\"deposit_min\": 0.4,\"withdraw_max\": 400,\"withdraw_min\": 400}]}";
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Content-Type", "application/json");
+        httpPost.setHeader("authorization",
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjY0LCJpc3MiOiJodHRwOi8vMTQ2LjcxLjc4LjIxMS9hcGkvbG9naW4iLCJpYXQiOjE1MzQxNzIzODMsImV4cCI6MTUzNDE3NTk4MywibmJmIjoxNTM0MTcyMzgzLCJqdGkiOiJXQmZweEJoSDNkYWpUUUs1In0.bsD6PYbKSD4aqTnvioXPCXYoKNjKvieiwxERic0tWNI");
+        CloseableHttpResponse response = client.execute(httpPost);
+        client.close();
+
+       /* String jsonPut = Request.Put("http://146.71.78.211/api/admin/fees")
           .addHeader("Content-Type", "application/json")
                 .addHeader("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjY0LCJpc3MiOiJodHRwOi8vMTQ2LjcxLjc4LjIxMS9hcGkvbG9naW4iLCJpYXQiOjE1MzQxNzIzODMsImV4cCI6MTUzNDE3NTk4MywibmJmIjoxNTM0MTcyMzgzLCJqdGkiOiJXQmZweEJoSDNkYWpUUUs1In0.bsD6PYbKSD4aqTnvioXPCXYoKNjKvieiwxERic0tWNI")
                    .bodyForm(Form.form()
@@ -45,9 +62,9 @@ public class AssetsTestFees {
 //                .bodyForm(new BasicNameValuePair("order_max", assetsFees.getOrder_max()))
 //                .bodyForm(new BasicNameValuePair("order_min", assetsFees.getOrder_min()))
                            .build()).execute()
-                                        .returnContent().asString();
+                                        .returnContent().asString();*/
 
-        System.out.println("!!!!!"+jsonPut);
+        System.out.println("!!!!!"+response);
         return 0;
 
     }
