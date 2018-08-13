12a1361a7c95932b5ed726a13fce958f896759e8
SlavaPihidko <slava17puh@gmail.com>
2018-08-13 18:46:47 +0300

    продолжение реализации Post запроса для AssetsFees

--- rest-sample/src/test/java/rest/AssetsFees.java
+++ rest-sample/src/test/java/rest/AssetsFees.java
@@ -1,14 +1,14 @@
 package rest;
 
 public class AssetsFees {
-    private int id;
-    private double order_max;
-    private double order_min;
-    private double deposit_max;
-    private double deposit_min;
-    private double withdraw_max;
-    private double withdraw_min;
-    private double withdraw_fee;
+    private String id;
+    private String order_max;
+    private String order_min;
+    private String deposit_max;
+    private String deposit_min;
+    private String withdraw_max;
+    private String withdraw_min;
+    private String withdraw_fee;
     private String st;
 
 
@@ -21,74 +21,74 @@ public class AssetsFees {
         return this;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public AssetsFees withId(int id) {
+    public AssetsFees withId(String id) {
         this.id = id;
         return this;
     }
 
-    public double getOrder_max() {
+    public String getOrder_max() {
         return order_max;
     }
 
-    public AssetsFees withOrder_max(double order_max) {
+    public AssetsFees withOrder_max(String order_max) {
         this.order_max = order_max;
         return this;
     }
 
-    public double getOrder_min() {
+    public String getOrder_min() {
         return order_min;
     }
 
-    public AssetsFees withOrder_min(double order_min) {
+    public AssetsFees withOrder_min(String order_min) {
         this.order_min = order_min;
         return this;
     }
 
-    public double getDeposit_max() {
+    public String getDeposit_max() {
         return deposit_max;
     }
 
-    public AssetsFees withDeposit_max(double deposit_max) {
+    public AssetsFees withDeposit_max(String deposit_max) {
         this.deposit_max = deposit_max;
         return this;
     }
 
-    public double getDeposit_min() {
+    public String getDeposit_min() {
         return deposit_min;
     }
 
-    public AssetsFees withDeposit_min(double deposit_min) {
+    public AssetsFees withDeposit_min(String deposit_min) {
         this.deposit_min = deposit_min;
         return this;
     }
 
-    public double getWithdraw_max() {
+    public String getWithdraw_max() {
         return withdraw_max;
     }
 
-    public AssetsFees withWithdraw_max(double withdraw_max) {
+    public AssetsFees withWithdraw_max(String withdraw_max) {
         this.withdraw_max = withdraw_max;
         return this;
     }
 
-    public double getWithdraw_min() {
+    public String getWithdraw_min() {
         return withdraw_min;
     }
 
-    public AssetsFees withWithdraw_min(double withdraw_min) {
+    public AssetsFees withWithdraw_min(String withdraw_min) {
         this.withdraw_min = withdraw_min;
         return this;
     }
 
-    public double getWithdraw_fee() {
+    public String getWithdraw_fee() {
         return withdraw_fee;
     }
 
-    public AssetsFees withWithdraw_fee(double withdraw_fee) {
+    public AssetsFees withWithdraw_fee(String withdraw_fee) {
         this.withdraw_fee = withdraw_fee;
         return this;
     }
--- rest-sample/src/test/java/rest/AssetsTestFees.java
+++ rest-sample/src/test/java/rest/AssetsTestFees.java
@@ -14,8 +14,16 @@ public class AssetsTestFees {
 
     @Test
     public void testAssetsTestFees() throws IOException {
-        AssetsFees assetsFees = new AssetsFees();
-        int assetsFeesId = updateAssetsFees(assetsFees);
+        AssetsFees assetsFees = new AssetsFees()
+                .withId("18")
+                .withOrder_max("100")
+                .withOrder_min("100")
+                .withDeposit_max("100")
+                .withOrder_min("0")
+                .withWithdraw_max("100")
+                .withWithdraw_min("100")
+                .withWithdraw_fee("0.001");
+        int assetsFeesId = updateAssetsFees(assetsFees); // эта строчка написана для того что бы вызвать метод updateAssetsFees
     }
 
 
@@ -24,11 +32,19 @@ public class AssetsTestFees {
                 String jsonPut = Request.Put("http://146.71.78.211/api/admin/fees")
           .addHeader("Content-Type", "application/json")
                 .addHeader("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjY0LCJpc3MiOiJodHRwOi8vMTQ2LjcxLjc4LjIxMS9hcGkvbG9naW4iLCJpYXQiOjE1MzQxNzIzODMsImV4cCI6MTUzNDE3NTk4MywibmJmIjoxNTM0MTcyMzgzLCJqdGkiOiJXQmZweEJoSDNkYWpUUUs1In0.bsD6PYbKSD4aqTnvioXPCXYoKNjKvieiwxERic0tWNI")
-                   .bodyForm(Form.form().add("user", "vfvf").build()).execute()
+                   .bodyForm(Form.form()
+                           .add("id", assetsFees.getId())
+                           .add("order_max",assetsFees.getOrder_max())
+                           .add("order_min",assetsFees.getOrder_min())
+                           .add("deposit_max",assetsFees.getDeposit_max())
+                           .add("deposit_min",assetsFees.getDeposit_min())
+                           .add("withdraw_max",assetsFees.getWithdraw_max())
+                           .add("withdraw_min",assetsFees.getWithdraw_min())
+                           .add("withdraw_fee",assetsFees.getWithdraw_fee())
 //                .bodyForm(new BasicNameValuePair("id", assetsFees.getId()))
 //                .bodyForm(new BasicNameValuePair("order_max", assetsFees.getOrder_max()))
 //                .bodyForm(new BasicNameValuePair("order_min", assetsFees.getOrder_min()))
-
+                           .build()).execute()
                                         .returnContent().asString();
 
         System.out.println("!!!!!"+jsonPut);
