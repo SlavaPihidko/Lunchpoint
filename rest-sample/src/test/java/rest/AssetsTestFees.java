c126c1b8f9f78ff387195572a1217c3a36f5b3cc
SlavaPihidko <slava17puh@gmail.com>
2018-08-14 21:40:22 +0300

    реализован Post запрос в котором получаем в ответе header и body

--- rest-sample/src/test/java/rest/AssetsTestFees.java
+++ rest-sample/src/test/java/rest/AssetsTestFees.java
@@ -1,5 +1,10 @@
 package rest;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
@@ -10,9 +15,12 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
 import java.util.Set;
 
 
@@ -36,15 +44,17 @@ public class AssetsTestFees {
     private int updateAssetsFees(AssetsFees assetsFees) throws IOException {
 
         CloseableHttpClient client = HttpClients.createDefault();
-        HttpPut httpPost = new HttpPut("http://146.71.78.211/api/admin/fees");
-        String json = "{\"assets\": [{\"id\":1,\"trade_fee\": 2,\"deposit_fee\": 0.4,\"withdraw_fee\": 0.000006,\"order_max\": 0.4,\"order_min\": 400,\"deposit_max\": 0.4,\"deposit_min\": 0.4,\"withdraw_max\": 400,\"withdraw_min\": 400}]}";
+        HttpPut httpPut = new HttpPut("http://146.71.78.211/api/admin/fees");
+        String json = "{\"assets\": [{\"id\":18,\"trade_fee\": 1,\"deposit_fee\": 1,\"withdraw_fee\": 0.000006,\"order_max\": 1000,\"order_min\": 0.0001,\"deposit_max\": 1000,\"deposit_min\": 0.00001,\"withdraw_max\": 100,\"withdraw_min\": 0.00001}]}";
         StringEntity entity = new StringEntity(json);
-        httpPost.setEntity(entity);
-        httpPost.setHeader("Content-Type", "application/json");
-        httpPost.setHeader("authorization",
-                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjY0LCJpc3MiOiJodHRwOi8vMTQ2LjcxLjc4LjIxMS9hcGkvbG9naW4iLCJpYXQiOjE1MzQxNzIzODMsImV4cCI6MTUzNDE3NTk4MywibmJmIjoxNTM0MTcyMzgzLCJqdGkiOiJXQmZweEJoSDNkYWpUUUs1In0.bsD6PYbKSD4aqTnvioXPCXYoKNjKvieiwxERic0tWNI");
-        CloseableHttpResponse response = client.execute(httpPost);
-        client.close();
+        httpPut.setEntity(entity);
+        httpPut.setHeader("Content-Type", "application/json");
+        httpPut.setHeader("authorization",
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjY0LCJpc3MiOiJodHRwOi8vMTQ2LjcxLjc4LjIxMS9hcGkvbG9naW4iLCJpYXQiOjE1MzQyNzAxOTcsImV4cCI6MTUzNDI3Mzc5NywibmJmIjoxNTM0MjcwMTk3LCJqdGkiOiJBSDE0MTBsN2FOUTVrelB4In0.CrZ08Gw1zsOqjHnTEVz-JMSRk8IXfP_sfZa7RFmJ_Lc");
+        CloseableHttpResponse responseHeader = client.execute(httpPut);
+        HttpEntity entity2 = responseHeader.getEntity();
+        String responseBody=  EntityUtils.toString(entity2);
+
 
        /* String jsonPut = Request.Put("http://146.71.78.211/api/admin/fees")
           .addHeader("Content-Type", "application/json")
@@ -64,7 +74,9 @@ public class AssetsTestFees {
                            .build()).execute()
                                         .returnContent().asString();*/
 
-        System.out.println("!!!!!"+response);
+        System.out.println("!!!!!"+responseHeader);
+        System.out.println("!!!!!"+responseBody);
+        client.close();
         return 0;
 
     }
